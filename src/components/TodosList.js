e29aac94fad3d4db109e56e6edf9efd1cfb7ec3c
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-19 03:17:11 +0500

    added buttona

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -3,13 +3,24 @@ const TodosList = ({ todos, setTodos }) => {
   return (
     <div>
       {todos.map((todo) => (
-        <li className="todo-list" key={todo.id}>
+        <li className="list-item" key={todo.id}>
           <input
             type="text"
             value={todo.title}
             className="list"
             onChange={(event) => event.preventDefault()}
           />
+          <div>
+            <button className="button-complete task-button">
+            <i className="fa fa-check-circle"></i>
+            </button>
+            <button className="button-complete task-button">
+            <i className="fa fa-edit"></i>
+            </button>
+            <button className="button-complete task-button">
+            <i className="fa fa-trash"></i>
+            </button>
+          </div>
         </li>
       ))}
     </div>
