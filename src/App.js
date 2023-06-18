1471b63959bef191db760da73d805f0703ec69c7
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-18 22:07:47 +0500

    added changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>React</h1>
+    <h1>r</h1>
     </div>
   );
 }
